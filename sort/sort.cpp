a7e2b70db24c6387c99abf8661e75b0e1ea948f7
Maciej Drozdowicz <drozdom0@AD.WSISIZ.EDU.PL>
2019-10-26 20:01:47 +0200

    Zastapienie funkcji lambdami

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -18,9 +18,9 @@ void scan(int &e) {
 
 int main() {
 	std::vector<int> vector(8);
-	std::for_each(vector.begin(), vector.end(), scan);
+	std::for_each(vector.begin(), vector.end(), [](int e) {std::cin >> e; });
 	std::sort(vector.begin(), vector.begin() + 4);
-	std::sort(vector.begin() + 4, vector.begin());
-	std::for_each(vector.begin(), vector.end(), print);
+	std::sort(vector.begin() + 4, vector.begin(), [](int e1, int e2) {return e1 > e2; });
+	std::for_each(vector.begin(), vector.end(), [](int e){std::cout << e << " "; });
 	std::cout << std::endl;
 }
